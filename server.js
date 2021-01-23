2331166b44d3c7ea6d849948657a7d6a45fa676e
Israel Guillermo <israelguillermo22@gmail.com>
2021-01-22 22:23:20 -0600

    deploying

--- server.js
+++ server.js
@@ -10,10 +10,15 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-mongoose.connect('mongodb://localhost/workout', {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/Myfitnessapp',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.use(require('./routes/html.js'));
 app.use(require('./routes/api.js'));
